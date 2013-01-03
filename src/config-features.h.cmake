e121c92603c49b3f8554708f1032264814ce749b
Ivan Čukić <ivan.cukic@kde.org>
2013-01-03 23:21:20 +0100

    Removed the encryption feature
    Removed components
     - Encryption jobs
     - UI plugins and UI system

--- src/config-features.h.cmake
+++ src/config-features.h.cmake
@@ -6,28 +6,6 @@
 
 #cmakedefine KAMD_DATA_DIR "@KAMD_DATA_DIR@"
 
-#cmakedefine FUSERMOUNT_PATH "@FUSERMOUNT_PATH@"
-
-#ifndef FUSERMOUNT_PATH
-    #define FUSERMOUNT_PATH "/bin/fusermount"
-#endif
-
-#cmakedefine ENCFS_PATH "@ENCFS_PATH@"
-
-#ifndef ENCFS_PATH
-    #define ENCFS_PATH "/usr/bin/encfs"
-#endif
-
-// #cmakedefine UI_HANDLER "@UI_HANDLER"
-
-#define KDIALOG_UI_HANDLER "activitymanager_uihandler_kdialog"
-#define DECLARATIVE_UI_HANDLER "activitymanager_uihandler_declarative"
-
-// #ifndef UI_HANDLER
-//     #define UI_HANDLER "activitymanager_uihandler_kdialog"
-//     #define UI_HANDLER "activitymanager_uihandler_declarative"
-// #endif
-
 #cmakedefine HAVE_CXX11_AUTO
 #cmakedefine HAVE_CXX11_NULLPTR
 #cmakedefine HAVE_CXX11_LAMBDA
