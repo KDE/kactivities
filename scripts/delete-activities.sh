cb5c048f3f1a8b1660dca62e92f19e8e7c2459de
Ivan Čukić <ivan.cukic@kde.org>
2014-05-05 23:21:03 +0200

    Removed deprecated scripts

--- scripts/delete-activities.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-alias nepomukcmd="sopranocmd --socket `kde4-config --path socket`nepomuk-socket --model main --nrl"
-for res in `nepomukcmd --foo query 'select ?r { ?r a kao:Activity . }'`; nepomukcmd rm $res
