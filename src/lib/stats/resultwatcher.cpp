b7b41e268c495df9a41bbc0db2c6d6f512955152
Ivan Čukić <ivan.cukic@kde.org>
2015-02-11 22:23:28 +0100

    Refactoring

--- src/lib/stats/resultwatcher.cpp
+++ src/lib/stats/resultwatcher.cpp
@@ -33,6 +33,7 @@
 #include <iterator>
 #include <functional>
 #include <mutex>
+#include <limits>
 #include <boost/optional.hpp>
 
 // KActivities
@@ -41,7 +42,10 @@
 #include "resourceslinking_interface.h"
 #include "resourcesscoring_interface.h"
 #include "common/dbus/common.h"
+#include "common/specialvalues.h"
 #include "activitiessync_p.h"
+#include "utils/lazy_val.h"
+#include "utils/qsqlquery_iterator.h"
 
 #include <boost/algorithm/cxx11/any_of.hpp>
 
@@ -49,11 +53,13 @@ namespace KActivities {
 namespace Experimental {
 namespace Stats {
 
+using boost::algorithm::any_of;
+
 // Main class
 
 class ResultWatcher::Private {
 public:
-    mutable ::Private::ConsumerPtr activities;
+    mutable ActivitiesSync::ConsumerPtr activities;
 
     Private(ResultWatcher *parent, Query query)
         : linking(new KAMD_DBUS_CLASS_INTERFACE(Resources/Linking, ResourcesLinking, Q_NULLPTR))
@@ -63,12 +69,62 @@ public:
     {
     }
 
+    // Processing the list of activities as specified by the query.
+    // If it contains :any, we are returning true, otherwise
+    // we want to match a specific activity (be it the current
+    // activity or not). The :global special value is not special here
     bool activityMatches(const QString &activity) const
     {
-        return true;
-        // return boost::any_of(query.activities(), [&] (const QString &matcher) {
-        //     if (matcher == Terms::Activity::global())
-        // });
+        return any_of(query.activities(), [&] (const QString &matcher) {
+            return (matcher == ANY_ACTIVITY_TAG)     ? true :
+                   (matcher == CURRENT_ACTIVITY_TAG) ? activity == ActivitiesSync::currentActivity(activities) :
+                                                       activity == matcher;
+        });
+    }
+
+    // Same as above, but for agents
+    bool agentMatches(const QString &agent) const
+    {
+        return any_of(query.agents(), [&] (const QString &matcher) {
+            return (matcher == ANY_AGENT_TAG)     ? true :
+                   (matcher == CURRENT_AGENT_TAG) ? agent == QCoreApplication::applicationName() :
+                                                    agent == matcher;
+        });
+    }
+
+    bool typeMatches(const QString &resource) const
+    {
+        // We don't necessarily need to retrieve the type from
+        // the database. If we do, get it only once
+        auto type = make_lazy_val([&] () -> QString {
+            using Common::Database;
+
+            auto query
+                = Database::instance(Database::ResourcesDatabase,
+                                     Database::ReadOnly)
+                      ->execQuery("SELECT mimetype FROM ResourceInfo WHERE "
+                                  "targettedResource = '" + resource + "'");
+
+            for (const auto &item : query) {
+                return item[0].toString();
+            }
+
+            return QString();
+        });
+
+        return any_of(query.types(), [&] (const QString &matcher) {
+            return matcher == ANY_TYPE_TAG || matcher == type;
+        });
+    }
+
+    bool eventMatches(const QString &agent, const QString &resource,
+                      const QString &activity) const
+    {
+        // The order of checks is not arbitrary, it is sorted
+        // from the cheapest, to the most expensive
+        return agentMatches(agent)
+               && activityMatches(activity)
+               && typeMatches(resource);
     }
 
     void linkResourceToActivity(const QString &agent, const QString &resource,
@@ -80,6 +136,10 @@ public:
 
         // The used resources do not really care about the linked ones
         if (query.selection() == Terms::UsedResources) return;
+
+        if (!eventMatches(agent, resource, activity)) return;
+
+        emit q->resultAdded(resource, std::numeric_limits<double>::infinity());
     }
 
     void unlinkResourceFromActivity(const QString &agent,
@@ -92,28 +152,26 @@ public:
 
         // The used resources do not really care about the linked ones
         if (query.selection() == Terms::UsedResources) return;
+
+        if (!eventMatches(agent, resource, activity)) return;
+
+        emit q->resultRemoved(resource);
     }
 
     void updateResourceScore(const QString &activity, const QString &agent,
                              const QString &resource, double score)
     {
-        Q_UNUSED(activity)
-
         Q_ASSERT_X(activity == "00000000-0000-0000-0000-000000000000" ||
                    !QUuid(activity).isNull(),
                    "ResultWatcher::updateResourceScore",
                    "The activity should be always specified here, no magic values");
 
-        if (!activityMatches(activity)) return;
-
-        Q_UNUSED(agent)
-        Q_UNUSED(resource)
-        Q_UNUSED(score)
-
         // The linked resources do not really care about the stats
         if (query.selection() == Terms::LinkedResources) return;
 
-        emit q->resourceScoreChanged(resource, score);
+        if (!eventMatches(agent, resource, activity)) return;
+
+        emit q->resultAdded(resource, score);
     }
 
 
