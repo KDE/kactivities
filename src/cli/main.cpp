e9767704da4d8ea6022bd626c75a420a617d8678
Ivan Čukić <ivan.cukic@kde.org>
2016-07-22 16:32:11 +0200

    Added a CLI application for controlling activities

---
+++ src/cli/main.cpp
@@ -0,0 +1,239 @@
+/*
+ *   Copyright (C) 2016 Ivan Cukic <ivan.cukic(at)kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <QCoreApplication>
+#include <QDebug>
+#include <QTimer>
+
+#include <KActivities/Controller>
+
+#include "utils.h"
+
+
+// Output modifiers
+
+DEFINE_COMMAND(bare, 0)
+{
+    flags.bare = true;
+    return 0;
+}
+
+DEFINE_COMMAND(noBare, 0)
+{
+    flags.bare = false;
+    return 0;
+}
+
+DEFINE_COMMAND(color, 0)
+{
+    flags.color = true;
+    return 0;
+}
+
+DEFINE_COMMAND(noColor, 0)
+{
+    flags.color = false;
+    return 0;
+}
+
+// Activity management
+
+DEFINE_COMMAND(createActivity, 1)
+{
+    auto result = awaitFuture(controller->addActivity(args(1)));
+
+    qDebug().noquote() << result;
+
+    return 1;
+}
+
+DEFINE_COMMAND(removeActivity, 1)
+{
+    awaitFuture(controller->removeActivity(args(1)));
+
+    return 1;
+}
+
+DEFINE_COMMAND(listActivities, 0)
+{
+    for (const auto& activity: controller->activities()) {
+        printActivity(activity);
+    }
+
+    return 0;
+}
+
+DEFINE_COMMAND(currentActivity, 0)
+{
+    printActivity(controller->currentActivity());
+
+    return 0;
+}
+
+// Activity switching
+
+DEFINE_COMMAND(setCurrentActivity, 1)
+{
+    switchToActivity(args(1));
+
+    return 1;
+}
+
+DEFINE_COMMAND(nextActivity, 0)
+{
+    const auto activities = controller->activities();
+    const auto currentActivity = controller->currentActivity();
+    bool found = false;
+
+    for (int i = 0; i < activities.count() - 1; ++i) {
+        if (activities[i] == currentActivity) {
+            found = true;
+            switchToActivity(activities[i + 1]);
+            break;
+        }
+    }
+
+    if (!found) {
+        switchToActivity(activities[0]);
+    }
+
+    return 0;
+}
+
+DEFINE_COMMAND(previousActivity, 0)
+{
+    const auto activities = controller->activities();
+    const auto currentActivity = controller->currentActivity();
+    bool found = false;
+
+    for (int i = 1; i < activities.count(); ++i) {
+        if (activities[i] == currentActivity) {
+            found = true;
+            switchToActivity(activities[i - 1]);
+            break;
+        }
+    }
+
+    if (!found) {
+        found = true;
+        switchToActivity(activities.last());
+    }
+
+    return 0;
+}
+
+void printHelp()
+{
+    if (!flags.bare) {
+        qDebug()
+            << "\nModifiers (applied only to trailing commands):"
+                 << "\n    --bare, --no-bare        - show minimal info vs show everything"
+                 << "\n    --color, --no-color      - make the output pretty"
+
+            << "\n\nCommands:"
+                 << "\n    --list-activities        - lists all activities"
+                 << "\n    --create-activity Name   - creates a new activity with the specified name"
+                 << "\n    --remove-activity ID     - removes the activity with the specified id"
+
+                 << "\n    --current-activity       - show the current activity"
+                 << "\n    --set-current-activity   - sets the current activity"
+                 << "\n    --next-activity          - switches to the next activity (in list-activities order)"
+                 << "\n    --previous-activity      - switches to the previous activity (in list-activities order)"
+                 ;
+
+    } else {
+        qDebug()
+                 << "\n--bare"
+                 << "\n--no-bare"
+                 << "\n--color"
+                 << "\n--no-color"
+                 << "\n--list-activities"
+                 << "\n--create-activitya NAME"
+                 << "\n--remove-activity ID"
+
+                 << "\n--current-activity"
+                 << "\n--set-current-activity"
+                 << "\n--next-activity"
+                 << "\n--previous-activity"
+                 ;
+
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    QTimer::singleShot(0, &app, [] {
+
+        const auto args = QCoreApplication::arguments();
+
+        controller = new KActivities::Controller();
+
+        while (controller->serviceStatus() != KActivities::Controller::Running) {
+            QCoreApplication::processEvents();
+        }
+
+        #define MATCH_COMMAND(Command)                                         \
+            else if (args[argId] == QStringLiteral("--") + toDashes(#Command)) \
+            {                                                                  \
+                argId += 1 + Command##_command({ args, argId })();             \
+            }
+
+        if (args.count() <= 1) {
+            printHelp();
+
+        } else for (int argId = 1; argId < args.count(); ) {
+            if (args[argId] == "--help") {
+                printHelp();
+                argId++;
+            }
+
+            MATCH_COMMAND(bare)
+            MATCH_COMMAND(noBare)
+            MATCH_COMMAND(color)
+            MATCH_COMMAND(noColor)
+
+            MATCH_COMMAND(listActivities)
+
+            MATCH_COMMAND(currentActivity)
+            MATCH_COMMAND(setCurrentActivity)
+            MATCH_COMMAND(nextActivity)
+            MATCH_COMMAND(previousActivity)
+
+            MATCH_COMMAND(createActivity)
+            MATCH_COMMAND(removeActivity)
+
+            else {
+                qDebug() << "Skipping unknown argument" << args[argId];
+                argId++;
+            }
+
+        }
+
+        delete controller;
+
+        QCoreApplication::quit();
+
+    });
+
+    return app.exec();
+}
+
---
+++ src/cli/utils.h
@@ -0,0 +1,168 @@
+/*
+ *   Copyright (C) 2016 Ivan Čukić <ivan.cukic(at)kde.org>
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2.1 of the License, or (at your option) version 3, or any
+ *   later version accepted by the membership of KDE e.V. (or its
+ *   successor approved by the membership of KDE e.V.), which shall
+ *   act as a proxy defined in Section 6 of version 3 of the license.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library.
+ *   If not, see <http://www.gnu.org/licenses/>.
+ */
+
+QTextStream out(stdout);
+
+class StringListView {
+public:
+    StringListView(const QStringList &list, int start, int end = -1)
+        : m_list(list)
+        , m_start(start)
+        , m_size((end == -1 ? list.count() : end) - start)
+    {
+
+    }
+
+    const QString &operator() (int index) const
+    {
+        return m_list[m_start + index];
+    }
+
+    int count() const
+    {
+        return m_size;
+    }
+
+private:
+    const QStringList &m_list;
+    int m_start;
+    int m_size;
+
+};
+
+KActivities::Controller *controller = nullptr;
+
+class Flags {
+public:
+    Flags()
+      : bare(false)
+      , color(true)
+    {
+    }
+
+    bool bare;
+    bool color;
+
+} flags;
+
+QString toDashes(const QString &command)
+{
+    QString result(command);
+
+    for (int i = 0; i < result.size() - 1; ++i) {
+        if (result[i].isLower() &&
+            result[i+1].isUpper()) {
+            result[i+1] = result[i+1].toLower();
+            result.insert(i+1, "-");
+        }
+    }
+
+    return result;
+}
+
+void printActivity(const QString &id)
+{
+    if (flags.bare) {
+        out << id << "\n";
+
+    } else {
+        using namespace KActivities;
+        Info info(id);
+
+        out
+            << (
+                info.id() == controller->currentActivity() ? "[CURRENT] " :
+                info.state() == Info::Running    ? "[RUNNING] " :
+                info.state() == Info::Stopped    ? "[STOPPED] " :
+                info.state() == Info::Starting   ? "[STARTING]" :
+                info.state() == Info::Stopping   ? "[STOPPING]" :
+                                                   "unknown   "
+            )
+            << info.id()
+            << " "
+            << info.name()
+            << " ("
+            << info.icon()
+            << ")\n"
+             ;
+
+        if (info.id() == controller->currentActivity()
+            && info.state() != Info::Running) {
+            qWarning()
+                 << "Activity is the current one, but its state is"
+                 << (
+                    info.state() == Info::Running  ? "running"  :
+                    info.state() == Info::Stopped  ? "stopped"  :
+                    info.state() == Info::Starting ? "starting" :
+                    info.state() == Info::Stopping ? "stopping" :
+                                                     "unknown   "
+                 );
+        }
+    }
+}
+
+template <typename T>
+T awaitFuture(const QFuture<T> &future)
+{
+    while (!future.isFinished()) {
+        QCoreApplication::processEvents();
+    }
+
+    return future.result();
+}
+
+void awaitFuture(const QFuture<void> &future)
+{
+    while (!future.isFinished()) {
+        QCoreApplication::processEvents();
+    }
+}
+
+void switchToActivity(const QString &id)
+{
+    auto result = awaitFuture(controller->setCurrentActivity(id));
+
+    if (!flags.bare) {
+        if (result) {
+            qDebug() << "Current activity is" << id;
+        } else {
+            qDebug() << "Failed to change the activity";
+        }
+    }
+}
+
+
+#define DEFINE_COMMAND(Command, MinArgCount)                                   \
+    struct Command##_command {                                                 \
+        const StringListView &args;                                            \
+        Command##_command(const StringListView &args)                          \
+            : args(args)                                                       \
+        {                                                                      \
+            if (args.count() < MinArgCount + 1) {                              \
+                qFatal("not enough arguments for " #Command);                  \
+            }                                                                  \
+        }                                                                      \
+                                                                               \
+        int operator()();                                                      \
+    };                                                                         \
+                                                                               \
+    int Command##_command::operator()()
+
