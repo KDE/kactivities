a5d904ae6d767f68034a36a93a5efce74c80407a
Christophe Giboudeaux <christophe@krop.fr>
2018-12-21 11:05:26 +0100

    Revert "Use ECMGeneratePkgConfigFile to create the pkgconfig file."
    The pkgconfig file name and the library name don't match for frameworks.
    If the LIB_NAME parameter is used, this will also replace the "Name:" content.
    
    This reverts commit 52267908a1e4bc968111e74bffc0cbf7f7f014c7.

---
+++ src/lib/libKActivities.pc.cmake
@@ -0,0 +1,12 @@
+prefix=${CMAKE_INSTALL_PREFIX}
+exec_prefix=${BIN_INSTALL_DIR}
+libdir=${LIB_INSTALL_DIR}
+includedir=${INCLUDE_INSTALL_DIR}
+
+Name: libKActivities
+Description: libKActivities is a C++ library for using KDE activities
+URL: http://www.kde.org
+Requires: Qt5Core
+Version: ${KACTIVITIES_LIB_VERSION_STRING}
+Libs: -L${LIB_INSTALL_DIR} -lKF5Activities
+Cflags: -I${INCLUDE_INSTALL_DIR}
