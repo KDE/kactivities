8c0352356062c83e2245c6d61bf1ebd3cdd6c73a
John Hayes <jdhayes.linux@gmail.com>
2019-12-05 10:34:13 +0100

    EBN KActivites transfer protocol cleanup
    Summary: Change transfer protocol from http to https
    
    Reviewers: apol
    
    Reviewed By: apol
    
    Subscribers: kde-frameworks-devel
    
    Tags: #frameworks
    
    Differential Revision: https://phabricator.kde.org/D25719

--- src/lib/libKActivities.pc.cmake
+++ src/lib/libKActivities.pc.cmake
@@ -5,7 +5,7 @@ includedir=${INCLUDE_INSTALL_DIR}
 
 Name: libKActivities
 Description: libKActivities is a C++ library for using KDE activities
-URL: http://www.kde.org
+URL: https://www.kde.org
 Requires: Qt5Core
 Version: ${KACTIVITIES_VERSION_STRING}
 Libs: -L${LIB_INSTALL_DIR} -lKF5Activities
